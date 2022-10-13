353f5fdeb45f32952c531196fcdd76644bdbebf5
Appoline Cogan <appolinecogan@gmail.com>
2022-10-14 10:53:47 +1100

    updated indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,17 +1,16 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
-
 
 export const getDb = async () => {
   console.log("GET from the database");
@@ -20,7 +19,7 @@ export const getDb = async () => {
   const store = tx.objectStore("jate");
   const request = store.getAll();
   const result = await request;
-  console.log("result.value", result);
+  console.log("Data retrieved from the database", result);
   return result;
 };
 
@@ -31,7 +30,7 @@ export const putDb = async (id, content) => {
   const store = tx.objectStore("jate");
   const request = store.put({ id: id, jateData: content });
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  console.log("🚀 - data saved to the database", result.jateData);
 };
 
 initdb();
