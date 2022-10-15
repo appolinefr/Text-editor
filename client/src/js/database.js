06fceab449acb16df9ec351de9afb2cf4b9249b1
Appoline Cogan <appolinecogan@gmail.com>
2022-10-16 09:46:06 +1100

    fixed db issues

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const getDb = async () => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log("Data retrieved from the database", result);
   return result;
@@ -28,7 +28,7 @@ export const putDb = async (id, content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({jateData: content });
+  const request = store.put({id: 1, value: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result.jateData);
 };
